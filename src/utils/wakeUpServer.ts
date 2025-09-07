e3898ce5eb104366431bc48295fb15d60594c79c
NightHunt04 <jeetbherwani2004@gmail.com>
2025-09-07 19:46:08 +0530

    minor change

--- src/utils/wakeUpServer.ts
+++ src/utils/wakeUpServer.ts
@@ -3,41 +3,47 @@ import axios from "axios"
 interface Response {
     status: string
     ready?: boolean
+    browser_instances?: Record<string, string>
 }
 
 export async function wakeUpServer() {
     try {
-        const res = await Promise.all([
-            axios.get<Response>(import.meta.env.VITE_BACKEND_HOST), 
-            axios.get<Response>(import.meta.env.VITE_BROWSER_URL_1),
-            axios.get<Response>(import.meta.env.VITE_BROWSER_URL_2),
-        ])
+        const res = await axios.get<Response>(import.meta.env.VITE_BACKEND_HOST)
 
-        if (res.some(r => r.data?.status !== "ok")) {
-            return { success: false, message: "Server is not running" }
+        if (res.data.status === 'ok' && res.data.browser_instances?.['https://playwright-browser-instance-1.onrender.com/'] === 'Running' && res.data.browser_instances?.['https://playwright-browser-instance.onrender.com/'] === 'Running') {
+            return { success: true, message: "Server is running and ready" }
         }
+        // const res = await Promise.all([
+        //     axios.get<Response>(import.meta.env.VITE_BACKEND_HOST), 
+        //     axios.get<Response>(import.meta.env.VITE_BROWSER_URL_1),
+        //     axios.get<Response>(import.meta.env.VITE_BROWSER_URL_2),
+        // ])
 
-        return new Promise<{ success: boolean; message: string }>((resolve) => {
-            const timeInter = setInterval(async () => {
-              try {
-                console.log('pinging browser instances')
-                const browserRes = await Promise.all([
-                  axios.get<Response>(import.meta.env.VITE_BROWSER_URL_1),
-                  axios.get<Response>(import.meta.env.VITE_BROWSER_URL_2),
-                ])
+        // if (res.some(r => r.data?.status !== "ok")) {
+        //     return { success: false, message: "Server is not running" }
+        // }
 
-                console.log(browserRes[0].data)
-                console.log(browserRes[1].data)
+        // return new Promise<{ success: boolean; message: string }>((resolve) => {
+        //     const timeInter = setInterval(async () => {
+        //       try {
+        //         console.log('pinging browser instances')
+        //         const browserRes = await Promise.all([
+        //           axios.get<Response>(import.meta.env.VITE_BROWSER_URL_1),
+        //           axios.get<Response>(import.meta.env.VITE_BROWSER_URL_2),
+        //         ])
+
+        //         console.log(browserRes[0].data)
+        //         console.log(browserRes[1].data)
       
-                if (browserRes[0].data?.ready && browserRes[1].data?.ready) {
-                  clearInterval(timeInter)
-                  resolve({ success: true, message: "Server is running and ready" })
-                }
-              } catch {
+        //         if (browserRes[0].data?.ready && browserRes[1].data?.ready) {
+        //           clearInterval(timeInter)
+        //           resolve({ success: true, message: "Server is running and ready" })
+        //         }
+        //       } catch {
                 
-              }
-            }, 5000)
-        })
+        //       }
+        //     }, 5000)
+        // })
     } catch (err) {
         return { success: false, message: "Failed to wake up server" }
     }
